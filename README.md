d7fec5edefd776d423d8d664faeb2cdf2dc678be
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-06-28 00:58:07 -0400

    Add usage instructions + basic test spec

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 stylish-commit
 ==============
 
+Usage:
+ 1. create a folder in your project for validation scripts (calling it `.style` saves you a step)
+
+ 1. add a field to package.json called "styleScripts" pointing at the folder (no need to do this if you called it `.style`)
+
+        package.json:
+        {
+          "name": "Foo barrer",
+          "version": "0.0.1",
+          ...
+          "styleScripts": "./scripts/style"
+        }
+
+ 1. fill it with scripts that look like:
+
+        .style/trailingWhitespace.js:
+        module.exports = {
+          name: 'Trailing whitespace',
+          description: 'Removes whitespace from the end of a line',
+          appliesTo: '*.js',
+          validate: function (changedLines) {
+            // Return lines the way they should look
+            return changedLines.map(function (line) { return line.replace(/\s+$/); });
+          };
+        };
+
+ 1. run `stylish-commit add-precommit` to run the scripts automatically before each commit
+
+    or
+
+    run `stylish-commit` to run on the current staged changes
--- test/index.js
+++ test/index.js
@@ -1,7 +1,23 @@
 var assert = require('chai').assert;
 
 describe('stylish-commit', function () {
-  it('should exist', function () {
-    assert(true);
+  describe('git integration', function () {
+    it('installs a git pre-commit hook');
+    it('only run scripts against staged commits');
+    it('lets you abort the commit if you ignore changes');
+  });
+
+  describe('finding script files', function () {
+    it('reads all scripts from the ".style" directory by default');
+    it('reads scripts from a custom directory when "styleScripts" is provided in package.json');
+  });
+
+  describe('validation', function () {
+    it('only calls a script\'s validate() method if the filename matches the "appliesTo" field');
+    it('detects differences between what validate() returns and what was provided');
+    it('allows validate() to return null, indicating there are no changes to be made');
+    it('prompts whether to apply the suggested changes');
+    it('lets you accept all changes, or iterate through individual changes');
+    it('writes changes to a file, and stages the changes');
   });
 });
