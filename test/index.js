8665c377b1e58cf3de951f8d551bdd9a5d2f2639
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-06-28 21:59:50 -0400

    Catch validation errors with scripts.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 var _ = require('lodash');
 var assert = require('chai').assert;
+var log = require('debug')('stylish-commit.test');
 var TemporaryRepository = require('./_helpers').Repository;
 var Repository = require('../lib/Repository.js');
 var ScriptLoader = require('../lib/ScriptLoader.js');
+var ScriptRunner = require('../lib/ScriptRunner.js');
 
 describe('stylish-commit', function () {
   describe('git integration', function () {
@@ -56,8 +58,65 @@ describe('stylish-commit', function () {
   });
 
   describe('validation', function () {
-    it('only calls a script\'s validate() method if the filename matches the "appliesTo" field');
-    it('detects differences between what validate() returns and what was provided');
+    it('only calls a script\'s validate() method if the filename matches the "appliesTo" field', function () {
+      var jsScriptCalled = 0;
+      var mdScriptCalled = 0;
+      var coffeeScriptCalled = 0;
+      var allScriptCalled = 0;
+      var scripts = [
+        { name: 'jsValidator',     appliesTo: '*.js',     validate: function (lines) { jsScriptCalled++;     return lines; } },
+        { name: 'mdValidator',     appliesTo: '*.md',     validate: function (lines) { mdScriptCalled++;     return lines; } },
+        { name: 'coffeeValidator', appliesTo: '*.coffee', validate: function (lines) { coffeeScriptCalled++; return lines; } },
+        { name: 'allValidator',                           validate: function (lines) { allScriptCalled++;    return lines; } }
+      ];
+
+      var diff = [
+        { file: 'foo.js', modifiedLines: [{ lineNumber: 3, text: 'foo' }] },
+        { file: 'bar.js', modifiedLines: [{ lineNumber: 1, text: 'bop' }] },
+        { file: 'bar.md', modifiedLines: [{ lineNumber: 1, text: 'bat' }] }
+      ];
+
+      var scriptRunner = new ScriptRunner(scripts);
+      scriptRunner.run(diff);
+
+      assert.equal(jsScriptCalled, 2);
+      assert.equal(mdScriptCalled, 1);
+      assert.equal(coffeeScriptCalled, 0);
+      assert.equal(allScriptCalled, 3);
+    });
+
+    it('detects differences between what validate() returns and what was provided', function () {
+      var scripts = [{
+        name: 'trailingLineTrimmer',
+        validate: function (lines) {
+          return lines.map(function (line) { return line.replace(/\s+$/, ''); });
+        }
+      }];
+
+      var diff = [
+        { file: 'foo.js', modifiedLines: [{ lineNumber: 1, text: '  foo   ' }] },
+        { file: 'bar.js', modifiedLines: [{ lineNumber: 2, text: 'bar' }, { lineNumber: 3, text: 'bar     ' }, { lineNumber: 4, text: ' bar    bar ' }] },
+        { file: 'bop.js', modifiedLines: [{ lineNumber: 4, text: 'bop' }] }
+      ];
+
+      var scriptRunner = new ScriptRunner(scripts);
+      var result = scriptRunner.run(diff);
+
+      assert.deepEqual(
+        result,
+        [{
+          file: 'foo.js',
+          results: [{ lineNumber: 1, text: '  foo   ', suggestions: [{ scriptName: 'trailingLineTrimmer', suggested: '  foo'}] }]
+        }, {
+          file: 'bar.js',
+          results: [
+            { lineNumber: 3, text: 'bar     ',     suggestions: [{ scriptName: 'trailingLineTrimmer', suggested: 'bar' }] },
+            { lineNumber: 4, text: ' bar    bar ', suggestions: [{ scriptName: 'trailingLineTrimmer', suggested: ' bar    bar' }] }
+          ]
+        }]
+      )
+    });
+
     it('allows validate() to return null, indicating there are no changes to be made');
     it('prompts whether to apply the suggested changes');
     it('lets you accept all changes, or iterate through individual changes');
