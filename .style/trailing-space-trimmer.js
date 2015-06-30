f44572066e2819bc24514c25e7bdc3b7f595cb87
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-06-30 15:03:05 -0400

    Update package.

--- .style/trailing-space-trimmer.js
+++ .style/trailing-space-trimmer.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'trailing-space-trimmer',
   validate: function (lines) {
-    return lines.map(function (line) { return line.replace(/\s+$/, ''); });  
+    return lines.map(function (line) { return line.replace(/\s+$/, ''); });
   }
 };
