741515f321352441f69574f10cdd1368887c183a
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-06-30 21:59:30 -0400

    Add test for automatic install.

--- test/_helpers.js
+++ test/_helpers.js
@@ -1,3 +1,4 @@
+var assert = require('chai').assert;
 var fs = require('fs-extra');
 var log = require('debug')('stylish-commit.test.helper');
 var path = require('path');
@@ -7,7 +8,7 @@ var Repository = function () {
   this.dirName = '/tmp/stylish_commit_test_' + Date.now() + '_' + (Math.random() * 1000 | 0);
   log('Creating ' + this.dirName);
   fs.mkdirSync(this.dirName);
-  this._run('git init');
+  this.runAtRoot('git init');
 };
 
 Repository.prototype = {
@@ -22,14 +23,16 @@ Repository.prototype = {
   containsFile: function (fileName) { return fs.existsSync(this._path(fileName)); },
 
   // Git actions
-  commit: function (commitMessage) { this._run('git commit -m "' + (commitMessage || 'committing') + '"'); },
-  stageFile: function (fileName) { this._run('git add "' + fileName + '"'); },
+  commit: function (commitMessage) { this.runAtRoot('git commit -m "' + (commitMessage || 'committing') + '"'); },
+  stageFile: function (fileName) { this.runAtRoot('git add "' + fileName + '"'); },
 
   // Misc
-  _run: function (cmd) {
+  runAtRoot: function (cmd) {
     var output = run(cmd, { cwd: this.dirName });
     log('Running `' + cmd + '`', output);
+
     if (output.status !== 0) {
+      assert.deepEqual(output.stderr, '');
       throw new Error('Error running "' + cmd + '":' + JSON.stringify(output));
     }
     return output.stdout;
--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ colors.enabled = false;
 var _ = require('lodash');
 var assert = require('chai').assert;
 var log = require('debug')('stylish-commit.test');
+var path = require('path');
 var Prompt = require('../lib/Prompt');
 var Repository = require('../lib/Repository');
 var ScriptLoader = require('../lib/ScriptLoader');
@@ -27,6 +28,21 @@ describe('stylish-commit', function () {
       assert.include(precommitFileContents, 'stylish-commit precommit hook');
 
       repository.uninstallPrecommitHook();
+      assert.notOk(temporaryRepository.containsFile('.git/hooks/pre-commit'));
+    });
+
+    it('automatically installs and removes a git pre-commit hook on npm install/remove', function () {
+      this.timeout(10000);
+      temporaryRepository.createFile('package.json', '{ "name": "test", "version": "0.0.1" }');
+
+      assert.notOk(temporaryRepository.containsFile('.git/hooks/pre-commit'));
+
+      var moduleRoot = path.dirname(__dirname);
+      temporaryRepository.runAtRoot('npm install --save "' + moduleRoot + '"');
+      var precommitFileContents = temporaryRepository.getFileContents('.git/hooks/pre-commit');
+      assert.include(precommitFileContents, 'stylish-commit precommit hook');
+
+      temporaryRepository.runAtRoot('npm uninstall --save stylish-commit');
       assert.notOk(temporaryRepository.containsFile('.git/hooks/pre-commit'));
     });
 
