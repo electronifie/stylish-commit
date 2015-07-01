aeada40a7f2a66b2a180ad0939e6426a9959b3c1
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-07-01 09:02:37 -0400

    Improve logging + fix application detection bug.

--- test/index.js
+++ test/index.js
@@ -229,7 +229,7 @@ describe('stylish-commit', function () {
     it('lets you abort the commit', function (done) {
       var suggestions = [{
         file: 'foo.js',
-        canApply: false,
+        canBeApplied: false,
         results: [{ lineNumber: 1, text: 'AA AA', suggestions: [{ scriptName: 'TEST_SCRIPT_1', suggested: 'AA BB AA' }] }]
       }, {
         file: 'bar.js',
